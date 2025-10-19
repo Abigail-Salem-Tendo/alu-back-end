597d487d711182b66ac6ebed4214ecbc20ae15e3
Abigail Salem Tendo <salemabigail10@gmail.com>
2025-10-19 21:59:55 +0200

    0-gather_data_from_an_API.py

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+This script uses a REST API to gather the TODO
+list progress for a given employee ID.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
+
+    #API endpoints
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        employee_id
+    )
+    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        employee_id
+    )
+
+    #Fetch data
+    user = requests.get(user_url).json()
+    todos = requests.get(todos_url).json()
+
+    employee_name = user.get("name")
+    total_tasks = len(todos)
+    done_tasks = [task for task in todos if task.get("done")]
+
+    print(
+        "Employee {} is done with ({}/{}):".format(
+            employee_name, len(done_tasks), total_tasks
+        )
+    )
+
+    for task in done_tasks:
+        print("\t{}".format(task.get("title")))
